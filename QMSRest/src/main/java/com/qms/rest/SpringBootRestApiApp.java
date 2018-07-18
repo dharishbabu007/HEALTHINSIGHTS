746dc261dad5888b02254f2dd7dd8fcf1cace889
Virendra Singh <30212@ITCINFOTECH.COM>
2018-07-18 12:31:16 +0530

    adding enable repository in SpringBootApp class

--- QMSRest/src/main/java/com/qms/rest/SpringBootRestApiApp.java
+++ QMSRest/src/main/java/com/qms/rest/SpringBootRestApiApp.java
@@ -2,11 +2,15 @@ package com.qms.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication(scanBasePackages={"com.qms.rest"})// same as @Configuration @EnableAutoConfiguration @ComponentScan combined
+@EnableJpaRepositories
+@EntityScan("com.qms.rest")
 public class SpringBootRestApiApp extends SpringBootServletInitializer {
 //public class SpringBootRestApiApp {
 
