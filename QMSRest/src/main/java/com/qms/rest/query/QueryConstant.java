df97601590b226a5212e55fc0fb100deac0166d5
Virendra Singh <30212@ITCINFOTECH.COM>
2018-07-25 16:24:20 +0530

    enhancement in member search based in firstName or lastName

--- QMSRest/src/main/java/com/qms/rest/query/QueryConstant.java
+++ QMSRest/src/main/java/com/qms/rest/query/QueryConstant.java
@@ -4,5 +4,7 @@ import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion;
 
 public class QueryConstant {
 
-    public static final String DIM_MEMBEER_QUERY ="SELECT dm.MEMBER_ID ,  dm.FIRST_NAME, dm.LAST_NAME  FROM DIM_MEMBER dm WHERE dm.MEMBER_ID = ? OR dm.FIRST_NAME = ?";
+    public static final String DIM_MEMBEER_QUERY_ID ="SELECT dm.MEMBER_ID ,  dm.FIRST_NAME, dm.LAST_NAME  FROM DIM_MEMBER dm WHERE dm.MEMBER_ID = ?";
+
+    public static final String DIM_MEMBEER_QUERY_NAME ="SELECT dm.MEMBER_ID ,  dm.FIRST_NAME, dm.LAST_NAME  FROM DIM_MEMBER dm WHERE dm.FIRST_NAME LIKE ? OR dm.LAST_NAME LIKE ?";
 }
--- QMSRest/src/main/java/com/qms/rest/service/MemberService.java
+++ QMSRest/src/main/java/com/qms/rest/service/MemberService.java
@@ -1,16 +1,20 @@
 package com.qms.rest.service;
 
+import com.qms.rest.exception.ProgramCreatorException;
 import com.qms.rest.model.DimMember;
-import com.qms.rest.repository.MemberRepository;
 import com.qms.rest.util.QMSConnection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.qms.rest.query.QueryConstant.DIM_MEMBEER_QUERY;
+import static com.qms.rest.query.QueryConstant.DIM_MEMBEER_QUERY_ID;
+import static com.qms.rest.query.QueryConstant.DIM_MEMBEER_QUERY_NAME;
 
 @Service
 public class MemberService {
@@ -18,30 +22,44 @@ public class MemberService {
     @Autowired
     private QMSConnection qmsConnection;
 
-    public List<DimMember> findDimMembers(String memberId)
-    {
+    public List<DimMember> findDimMembers(String memberId) {
         List<DimMember> memberList = null;
-        System.out.println("Get Member for given member Id: "+memberId);
+        System.out.println("Get Member for given member Id: " + memberId);
 
         PreparedStatement statement = null;
         ResultSet resultSet = null;
         Connection connection = null;
 
+        boolean isNumeric = memberId.chars().allMatch(Character::isDigit);
+
+        String sqlQuery = isNumeric ? DIM_MEMBEER_QUERY_ID : DIM_MEMBEER_QUERY_NAME;
+
         try {
             connection = qmsConnection.getHiveConnection();
             System.out.println("Service after connection: " + connection);
-            statement = connection.prepareStatement(DIM_MEMBEER_QUERY);
-            statement.setString(1, memberId);
-            statement.setString(2, memberId);
+            statement = connection.prepareStatement(sqlQuery);
+            if(isNumeric){
+                statement.setString(1, memberId);
+            }
+            else{
+                memberId = "%"+memberId+"%";
+                statement.setString(1, memberId);
+                statement.setString(2, memberId);
+            }
             resultSet = statement.executeQuery();
             memberList = convertToDimMember(resultSet);
 
         } catch (Exception e) {
-            System.out.println("Error: Exception while fetching dimMember for id: "+memberId);
+            System.out.println("Error: Exception while fetching dimMember for id: " + memberId);
             e.printStackTrace();
         } finally {
             System.out.println("About to close connection in find dim members");
             qmsConnection.closeJDBCResources(resultSet, statement, connection);
+        }
+
+        if (memberList.size() == 0) {
+            System.out.println("Warn: There is no entry found for given search key: " + memberId);
+            throw new ProgramCreatorException("There is no entry found for given search key: " + memberId);
         }
         return memberList;
     }
@@ -49,7 +67,7 @@ public class MemberService {
     private List<DimMember> convertToDimMember(ResultSet resultSet) throws SQLException {
         List<DimMember> memberList = new ArrayList<>();
 
-        while (resultSet.next()){
+        while (resultSet.next()) {
             DimMember dimMember = DimMember.builder()
                     .firstName(resultSet.getString("FIRST_NAME"))
                     .lastName(resultSet.getString("LAST_NAME"))
@@ -57,7 +75,7 @@ public class MemberService {
                     .build();
             memberList.add(dimMember);
         }
-        System.out.println("Total fetched record : "+memberList.size());
+        System.out.println("Total fetched record : " + memberList.size());
         return memberList;
     }
 
