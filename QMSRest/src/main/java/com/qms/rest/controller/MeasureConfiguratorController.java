59aa892ed13152de18e962ef0b3296e6b8646370
raghukonda <raghunadha.konda@itcinfotech.com>
2018-07-27 15:58:50 +0530

    added CrossOrigin in controllers

--- QMSRest/src/main/java/com/qms/rest/controller/MeasureConfiguratorController.java
+++ QMSRest/src/main/java/com/qms/rest/controller/MeasureConfiguratorController.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +25,7 @@ import com.qms.rest.util.CustomErrorType;
 
 @RestController
 @RequestMapping("/measure_configurator")
+@CrossOrigin
 public class MeasureConfiguratorController {
 	
 	@Autowired
