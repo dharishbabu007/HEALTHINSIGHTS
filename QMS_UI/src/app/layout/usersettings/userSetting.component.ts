fba69828efd405b0801de19f2ab8151b8a50fc6f
Navyapasupuleti <Navya.pasupuleti@itcinfotech.com>
2018-09-04 17:43:45 +0530

    User settings integration, changes till 04/09

--- QMS_UI/src/app/layout/usersettings/userSetting.component.ts
+++ QMS_UI/src/app/layout/usersettings/userSetting.component.ts
@@ -2,12 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { UserSettingService } from './userSetting.service';
 import { FormGroup, FormControl, Validators, FormBuilder, FormArray } from '@angular/forms';
 import { Router } from '@angular/router';
+
+import { GapsService } from '../../shared/services/gaps.service';
+import { MessageService } from '../../shared/services/message.service';
 import { UserSetting } from './userSetting.component';
 
 @Component({
   selector: 'app-userSetting',
   templateUrl: './userSetting.component.html',
-  styleUrls: ['./userSetting.component.scss']
+  styleUrls: ['./userSetting.component.scss'],
+    providers: [GapsService]
 })
 export class UserSettingComponent implements OnInit {
 
@@ -15,11 +19,17 @@ export class UserSettingComponent implements OnInit {
 
   public submitted: boolean;
 
+  QuestionList: any;
+
   constructor(private _fb: FormBuilder,
     private UserSettingService: UserSettingService,
-    private router: Router) { }
+    private router: Router,
+     private gapsService: GapsService,
+    private msgService: MessageService,) { }
 
   ngOnInit() {
+
+
       this.myForm = this._fb.group({
         firstName: [''],
         lastName: [''],
@@ -29,6 +39,15 @@ export class UserSettingComponent implements OnInit {
         EmailId: ['']
         
       });
+
+  this.gapsService.getSecurityQuestions().subscribe((data: any) => {
+    this.QuestionList = [];
+    data.forEach(element => {
+      this.QuestionList.push({label: element.name, value: element.name});
+    });
+  });
+
+
   }
 
   submitPc(modelPc: UserSetting, isValid: boolean) {
@@ -37,20 +56,12 @@ export class UserSettingComponent implements OnInit {
       // call API to save
       // ...
       console.log( 'Model' + JSON.stringify(modelPc));
-    this.UserSettingService.UserSettingSubmit(modelPc).subscribe( model => console.log('Succeessfully Created Program Creator'));
+    this.UserSettingService.UserSettingSubmit(modelPc).subscribe( model => console.log('Succeessfully Updates UserSettings'));
     this.router.navigateByUrl('/dashboard');
   }
 
-  savePc(modelPC: UserSetting, isValid: boolean) {
-
-    this.submitted = true;
-   // call API to save
-   // ...
-   console.log('Model SavePC ' + JSON.stringify(modelPC));
- this.UserSettingService.UserSettingSubmit(modelPC).subscribe( model => console.log('Succeessfully Created Program Creator'));
-}
-
+  
 }
 export interface UserSetting{
-  
+  securityQuestion: string;
 }
--- QMS_UI/src/app/layout/usersettings/userSetting.service.ts
+++ QMS_UI/src/app/layout/usersettings/userSetting.service.ts
@@ -5,6 +5,7 @@ import { catchError } from 'rxjs/operators';
 import { HttpErrorHandler, HandleError } from '../../shared/services/http-error-handler.service';
 import { UserSetting } from './userSetting.component';
 
+
 const httpOptions = {
   headers: new HttpHeaders({
     'Access-Control-Allow-Origin': '*',
@@ -12,7 +13,7 @@ const httpOptions = {
   })
 };
 
-
+ 
 @Injectable({
   providedIn: 'root'
 })
@@ -22,18 +23,20 @@ export class UserSettingService {
   private handleError: HandleError;
 
   constructor(private http: HttpClient,
-    httpErrorHandler: HttpErrorHandler) {
+    httpErrorHandler: HttpErrorHandler,
+   ) {
       this.handleError =  httpErrorHandler.createHandleError('UserSettingService');
      }
+
+ 
   UserSettingSubmit(model: UserSetting): Observable<UserSetting> {
         console.log('UserSetting');
 
-        return this.http.post<UserSetting>('http://healthinsight:8082/curis/qms/createProgram', model, httpOptions)
+        return this.http.post<UserSetting>('http://healthinsight:8082/curis/user/update_user/', model, httpOptions)
         . pipe(
             catchError(this.handleError('UserSettingSubmit', model))
           );
        // this.http.post('http://<hostname>:<port>/curis/qms/createProgram',model);
   }
-
 
 }
