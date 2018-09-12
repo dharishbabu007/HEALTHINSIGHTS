327b441f6bb266e59c488935a548b10cc3c51bec
raghukonda <raghunadha.konda@itcinfotech.com>
2018-09-12 12:21:40 +0530

    Change the response of create program

--- QMSRest/src/main/java/com/qms/rest/controller/QMSController.java
+++ QMSRest/src/main/java/com/qms/rest/controller/QMSController.java
@@ -13,6 +13,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -212,12 +213,15 @@ public class QMSController {
 
 	@RequestMapping(value = "/createProgram", method = RequestMethod.POST)
 	@ResponseBody
-	public ResponseEntity<?> createProgram(@RequestBody Program program) {
+	public ResponseEntity<RestResult> createProgram(@RequestBody Program program) {
 		String response = "Program created successfully for ProgramName : "+program.getProgramName();
 		logger.info("About to create program :  " + program);
 		programService.createProgram(program);
-		HttpHeaders headers = new HttpHeaders();
-		return new ResponseEntity<String>(response, headers, HttpStatus.OK);
+		
+		RestResult restResult = RestResult.getSucessRestResult(response);		
+//		HttpHeaders headers = new HttpHeaders();
+//		headers.setContentType(MediaType.TEXT_PLAIN);
+		return new ResponseEntity<RestResult>(restResult, HttpStatus.OK);
 	}
 
 //	@RequestMapping(value = "/caregaps", method = RequestMethod.GET)
