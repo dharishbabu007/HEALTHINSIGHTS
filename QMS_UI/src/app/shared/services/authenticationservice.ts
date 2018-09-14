63c8f0004db6458b407f84a6a7d04d973e8ba9ef
Navyapasupuleti <Navya.pasupuleti@itcinfotech.com>
2018-09-14 19:09:33 +0530

    Error Message

--- QMS_UI/src/app/shared/services/authenticationservice.ts
+++ QMS_UI/src/app/shared/services/authenticationservice.ts
@@ -1,14 +1,42 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
 import { map } from 'rxjs/operators';
+
+import {
+    HttpRequest,
+    HttpHandler,
+    HttpEvent,
+    HttpInterceptor, HttpErrorResponse
+  } from '@angular/common/http';
+  import {Observable, of} from 'rxjs';
+  import {Router} from "@angular/router";
+  import {catchError} from "rxjs/internal/operators";
 
 @Injectable()
 export class AuthenticationService {
-    constructor(private http: HttpClient) { }
-    
+    constructor(private http: HttpClient, private router: Router) { }
+    private handleAuthError(err: HttpErrorResponse): Observable<any> {
+        //handle your auth error or rethrow
+        if (err.status === 401) {
+          //navigate /delete cookies or whatever
+          console.log('handled error ' + err.status);
+          this.router.navigate([`/login`]);
+          // if you've caught / handled the error, you don't want to rethrow it unless you also want downstream consumers to have to handle it as well.
+          return of(err.message);
+        }
+        throw Error;
+      }
+
     login(username: string, password: string) {
         return this.http.get<any>(`http://healthinsight:8082/curis/user/login/${username}/${password}`)
             .pipe(
+                catchError((error, caught) => {
+                    //intercept the respons error and displace it to the console
+                   alert(error.error.errorMessage);
+                    this.handleAuthError(error);
+                    return of(error);
+                  }),
                 map(user => {
                 // login successful if there's a jwt token in the response
                 
