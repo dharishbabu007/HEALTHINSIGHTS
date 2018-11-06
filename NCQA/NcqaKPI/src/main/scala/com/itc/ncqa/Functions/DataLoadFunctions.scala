1098cec79e6a2af186f1291548c68c60d8212c15
Sangeeth KR <25865@ITCINFOTECH.COM>
2018-11-06 17:59:15 +0530

    ABA,ADV,AWC data rework done

--- NCQA/NcqaKPI/src/main/scala/com/itc/ncqa/Functions/DataLoadFunctions.scala
+++ NCQA/NcqaKPI/src/main/scala/com/itc/ncqa/Functions/DataLoadFunctions.scala
@@ -6,9 +6,15 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 object DataLoadFunctions {
 
 
-
-
-
+  /**
+    *
+    * @param spark (SparkSession Object)
+    * @param dbName (database name)
+    * @param tblName (table name)
+    * @param sourceName (source name which get from the program argument)
+    * @return dataframe that laods the data which has the source name that we have passed from the coreesponding hive table.
+    * @usecase Function is used to laod the dim and afact tables from the targeted Hive table
+    */
   def dataLoadFromTargetModel(spark:SparkSession,dbName:String,tblName:String,sourceName:String):DataFrame ={
 
     val source_name = "'"+sourceName+"'"
@@ -21,6 +27,14 @@ object DataLoadFunctions {
   }
 
 
+  /**
+    *
+    * @param spark (SparkSession Object)
+    * @param dbName (database name)
+    * @param tblName (Table name)
+    * @return Datfarme that conatins the data from thae corresponding Hive table
+    * @usecase Function is used to load the ref tables.
+    */
   def referDataLoadFromTragetModel(spark:SparkSession,dbName:String,tblName:String):DataFrame ={
 
     val query = "select * from "+ dbName+"."+ tblName
@@ -31,12 +45,25 @@ object DataLoadFunctions {
   }
 
 
+  /**
+    *
+    * @param spark (SparkSession Object)
+    * @return dataframe that contains the date_sk and calender_date
+    * @usecase This function is used to load the date_sk and calender_date from the dim_date table.
+    */
   def dimDateLoadFunction(spark:SparkSession):DataFrame ={
     val dimDateDf = spark.sql(KpiConstants.dimDateLoadQuery)
     dimDateDf
   }
 
 
+  /**
+    *
+    * @param spark (SparkSession Object)
+    * @param measureTitle (Measure title which get from the program argument)
+    * @return Dataframe that contains the quality_measure_sk for the measure title
+    * @usecase This functio ius used to find the quality_measure_sk for the measure title that passed as argument
+    */
   def qualityMeasureLoadFunction(spark:SparkSession,measureTitle:String):DataFrame ={
 
     val measure_title = "'"+measureTitle+"'"
@@ -46,6 +73,13 @@ object DataLoadFunctions {
   }
 
 
+  /**
+    *
+    * @param spark (SparkSession Object)
+    * @param viewName (view name that has to call)
+    * @return dataframe that contains the view data
+    * @usecase This function is used to call the view name and loads the view data to dataframe
+    */
   def viewLoadFunction(spark:SparkSession,viewName:String):DataFrame = {
 
     val newDf = viewName match {
