e8d9728bccb5f084775f3e63e8ce6aeeeb0ad7f4
raghukonda <raghunadha.konda@itcinfotech.com>
2018-12-20 15:45:18 +0530

    Changed as per maven

--- QMSRest/src/main/java/com/qms/rest/service/MemberEngagementService.java
+++ QMSRest/src/main/java/com/qms/rest/service/MemberEngagementService.java
@@ -6,6 +6,8 @@ import com.qms.rest.model.ClusterAnalysis;
 import com.qms.rest.model.ClusterData;
 import com.qms.rest.model.ClusterPersona;
 import com.qms.rest.model.ConfusionMatric;
+import com.qms.rest.model.LHEOutput;
+import com.qms.rest.model.ModelMetric;
 import com.qms.rest.model.ModelScore;
 import com.qms.rest.model.ModelSummary;
 import com.qms.rest.model.RestResult;
@@ -20,4 +22,10 @@ public interface MemberEngagementService {
 	RestResult updateClusteringPersona (ClusterPersona clusteringPersona);
 	ClusterData getClusteringData (int clusterId);
 	
+	Set<LHEOutput> getLHEModelOutPut();
+	Set<ModelSummary> getLHEModelSummary();	
+	ModelMetric getLHEModelMetric();	
+	String[][] getLHEReasonNotEnrollStatics();
+	
+	RestResult createLHEInputFile();
 }
